efe9ee7f7e37670445975fd60ddb427c090c4809
mdscr3 <160481957+mdscr3@users.noreply.github.com>
2024-05-21 16:06:22 +0200

    Initiële bestanden voor MakeCode-project

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Open deze pagina op [https://mdscr3.github.io/maqueen-remote-control/](https://mdscr3.github.io/maqueen-remote-control/)
+
+## Gebruiken als extensie
+
+Deze repository kan worden toegevoegd als **extensie** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* klik op **Nieuw project**
+* klik op **Extensies** onder het tandwielmenu
+* zoeken naar **https://github.com/mdscr3/maqueen-remote-control** en importeren
+
+## Dit project bewerken
+
+Om deze repository te bewerken in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* klik op **Importeren** en klik vervolgens op **Importeer URL**
+* plak **https://github.com/mdscr3/maqueen-remote-control** en klik op importeren
+
+#### Metadata (gebruikt voor zoeken, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,56 @@
+let ir_value = 0
+let vooruit = 94
+let achteruit = 95
+let links = 93
+let rechts = 92
+let stop = 37
+basic.forever(function () {
+    ir_value = IR.IR_read()
+})
+basic.forever(function () {
+    if (ir_value == vooruit) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+    if (ir_value == achteruit) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (ir_value == links) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+    if (ir_value == rechts) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+    if (ir_value == stop) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
+})
